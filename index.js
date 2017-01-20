0ab87ce60c6f51a0dc891a0937dc2286dd25fb5c
Daniel Homer <danielhomer@me.com>
2017-01-20 13:38:18 +0000

    First working example

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 'use strict';
 
+process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
+
 const exec = require('child_process').exec;
-const execDir = 'Lambda-Tesseract/'
+const execDir = process.env['LAMBDA_TASK_ROOT'] + '/Tesseract-CentOS'
 const executable = 'tesseract'
-const defines = 'LD_LIBRARY_PATH=' + execDir + 'lib TESSDATA_PREFIX=' + execDir;
+const defines = 'LD_LIBRARY_PATH=' + execDir + '/lib TESSDATA_PREFIX=' + execDir;
 const args = '';
 
 exports.handler = (event, context, callback) => {
-    let command = defines + ' ' + execDir + '/' + executable + ' ' + args;
+    let command = defines + ' ' + execDir + '/' + executable;
+    let args = 'test.png stdout';
     
-    process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
-    
-    exec(command, (error, stdout, stderr) => {
+    exec(command + ' ' + args, (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`);
         return;
